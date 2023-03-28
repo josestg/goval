100b5c87ac58b1f6009a0a88454e661347ed30af
Jose Sitanggang <josealfredositanggang@gmail.com>
2023-03-28 23:35:10 +0700

    test: add test for goval.TextError.

--- errors_test.go
+++ errors_test.go
@@ -6,6 +6,15 @@ import (
 	"testing"
 )
 
+func TestTextError(t *testing.T) {
+	err := goval.TextError("my-error")
+	exp := "my-error"
+	got := err.Error()
+	if got != exp {
+		t.Errorf("expect string Error: %q; got %q", exp, got)
+	}
+}
+
 func TestKeyError(t *testing.T) {
 	t.Run("Error: using marshal able error", func(t *testing.T) {
 		err := goval.KeyError{
